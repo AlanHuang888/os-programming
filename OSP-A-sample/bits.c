7b9e5af90ac6b5f21a54fedda378961a060a39bd
Alan <18004045@student.westernsydney.edu.au>
2021-03-15 16:09:24 +1100

    added template code

---
+++ OSP-A-sample/bits.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdint.h>
+#include"bits.h"
+
+void print_bits(uint8_t value)
+{
+
+}
+
+uint8_t reverse_bits(uint8_t value)
+{
+
+}
+
+uint8_t check_bit(uint8_t value, uint8_t bit)
+{
+
+}
+
+uint8_t toggle_bit(uint8_t *value, uint8_t bit)
+{
+
+}
+
+uint8_t get_subfield(uint8_t value, uint8_t start, uint8_t stop)
+{
+
+}
